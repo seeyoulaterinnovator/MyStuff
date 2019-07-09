62a2847f848c4f1451e82b22e29e6e19654d1b53
delow <delow1990@gmail.com>
2019-07-09 18:19:38 +0500

    SSO-22 Привязка библиотеки для тестирования

--- adapter/tbapi-remote-service/src/test/java/ru/alamics/sso/remote/tbapi/TbapiMockServiceTest.java
+++ adapter/tbapi-remote-service/src/test/java/ru/alamics/sso/remote/tbapi/TbapiMockServiceTest.java
@@ -1,5 +1,7 @@
 package ru.alamics.sso.remote.tbapi;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Rule;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.alamics.sso.registration.model.TbapiRequest;
@@ -7,9 +9,8 @@ import ru.alamics.sso.registration.model.TbapiRequest;
 import java.util.Map;
 import java.util.UUID;
 
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.entry;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 class TbapiMockServiceTest {
@@ -21,8 +22,12 @@ class TbapiMockServiceTest {
         service = new TbapiMockService();
     }
 
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().dynamicPort());
+
     @Test
     void createLead() {
+        int port = wireMockRule.port();
         Map<String, String> lead = service.createLead(TbapiRequest.builder()
                 .id(UUID.randomUUID().toString())
                 .email("test@test.ru")
