a6809d27865c78bf354d52b5d36a043b42377b66
tretyakov-da <tretyakov-da@alamics.ru>
2019-09-04 23:13:59 +0500

    Проемужуточный коммит

---
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/form/AttributesFormFactory.java
@@ -0,0 +1,81 @@
+package ru.alamics.sso.keycloak.auth.form;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.List;
+
+public class AttributesFormFactory implements AuthenticatorFactory {
+    public static final String PROVIDER_ID = "attributes-form";
+    private static final AttributesForm FORM = new AttributesForm();
+
+    private static final AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
+            AuthenticationExecutionModel.Requirement.REQUIRED,
+            AuthenticationExecutionModel.Requirement.DISABLED
+    };
+    @Override
+    public String getDisplayType () {
+        return "User selection post form";
+    }
+
+    @Override
+    public String getReferenceCategory () {
+        return null;
+    }
+
+    @Override
+    public boolean isConfigurable () {
+        return false;
+    }
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices () {
+        return REQUIREMENT_CHOICES;
+    }
+
+    @Override
+    public boolean isUserSetupAllowed () {
+        return false;
+    }
+
+    @Override
+    public String getHelpText () {
+        return "";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties () {
+        return null;
+    }
+
+    @Override
+    public Authenticator create (KeycloakSession session) {
+
+        return FORM;
+    }
+
+    @Override
+    public void init (Config.Scope config) {
+
+    }
+
+    @Override
+    public void postInit (KeycloakSessionFactory factory) {
+
+    }
+
+    @Override
+    public void close () {
+
+    }
+
+    @Override
+    public String getId () {
+        return PROVIDER_ID;
+    }
+}
