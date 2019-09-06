5a3314f57c9994ee03444861cd320aa6269f4d0c
tretyakov-da <tretyakov-da@alamics.ru>
2019-09-06 17:55:43 +0500

    SSO-108 - log get params

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/post/AttributesForm.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/post/AttributesForm.java
@@ -34,7 +34,9 @@ public class AttributesForm implements Authenticator {
         var authSession = context.getAuthenticationSession();
         log.info("{}: frame={}", DEBUG_STR, authSession.getAuthNote(I_FRAME));
         var uriInfo = context.getUriInfo();
-        log.info("{}: getParams={}", DEBUG_STR, uriInfo.getQueryParameters());
+        var queryParams = uriInfo.getQueryParameters();
+        queryParams.forEach((key, value) -> log.info("{}: key={} value={}", DEBUG_STR, key, value));
+
         String frame = uriInfo.getQueryParameters().getFirst(I_FRAME);
         boolean isAuth = "1".equals(authSession.getAuthNote(AUTH_FORM_SUCCESS));//it`s magick
 
