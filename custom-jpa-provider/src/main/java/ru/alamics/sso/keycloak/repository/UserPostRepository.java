ce79e24d5b58d652994bad459e0aee390c5a2ac0
Плюснин Дмитрий Анатольевич <plusnin-da@it-rev.ru>
2019-09-12 06:56:46 +0000

    Revert "Merge branch 'SSO-110' into 'develop'"
    This reverts merge request !46

--- custom-jpa-provider/src/main/java/ru/alamics/sso/keycloak/repository/UserPostRepository.java
+++ custom-jpa-provider/src/main/java/ru/alamics/sso/keycloak/repository/UserPostRepository.java
@@ -2,8 +2,6 @@ package ru.alamics.sso.keycloak.repository;
 
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.models.jpa.entities.UserEntity;
-import ru.alamics.sso.keycloak.entity.ExternalSystem;
-import ru.alamics.sso.keycloak.entity.ExternalSystemRole;
 import ru.alamics.sso.keycloak.entity.UserPost;
 import ru.alamics.sso.keycloak.entity.UserPostRole;
 
@@ -87,31 +85,5 @@ public class UserPostRepository {
                 .setParameter("user", user)
                 .getResultList();
         return ret;
-    }
-
-    public List<ExternalSystem> getAllExternalSystem(){
-        return em.createQuery(
-                "select sys " +
-                        "from ExternalSystem sys", ExternalSystem.class)
-                .getResultList();
-    }
-
-    public List<ExternalSystemRole> getAllExternalSystemRole(){
-        return em.createQuery(
-                "select role " +
-                        "from ExternalSystemRole role", ExternalSystemRole.class)
-                .getResultList();
-    }
-
-    public UserPost addSystemRole(UserPost userPost, Long extSystemRoleId){
-        userPost.getSystemRoles().add(em.find(ExternalSystemRole.class, extSystemRoleId));
-        update(userPost);
-        return userPost;
-    }
-
-    public UserPost removeSystemRole(UserPost userPost, Long extSystemRoleId){
-        userPost.getSystemRoles().remove(em.find(ExternalSystemRole.class, extSystemRoleId));
-        update(userPost);
-        return userPost;
     }
 }
