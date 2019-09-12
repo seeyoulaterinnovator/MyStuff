a373623e221845318c0313b6217969c0e6ad64a1
Плюснин Дмитрий Анатольевич <plusnin-da@alamics.ru>
2019-09-12 22:33:57 +0500

    battle flag

--- adapter/tbapi-remote-service/src/main/java/ru/alamics/sso/remote/viber/ViberSendServiceImpl.java
+++ adapter/tbapi-remote-service/src/main/java/ru/alamics/sso/remote/viber/ViberSendServiceImpl.java
@@ -56,7 +56,7 @@ public class ViberSendServiceImpl implements ViberSendService {
     public String sendMsg(String phone, String text) {
 
         // локально и на дэве фиксированный код и не отправляю смс
-        if (!StandResolver.ENV.isBattle()) {
+        if (!StandResolver.isBattle()) {
             log.info("Stand {}, do not sending viber msg", StandResolver.ENV);
             return "0: Accepted for delivery";
         }
