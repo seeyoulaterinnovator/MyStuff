c312def81cd8f766b28f9943a8ff9be572caa07a
mazeina-nn <mazeina-nn@alamics.ru>
2019-09-16 11:45:47 +0500

    Ф - неправильно отображается иконка "Чат"
    Некорректно отображается иконка чата в браузерах и моб адаптиве - поаправлено

--- keycloak-extension/src/main/resources/themes/domru/login/resources/src/Cities/index.svelte
+++ keycloak-extension/src/main/resources/themes/domru/login/resources/src/Cities/index.svelte
@@ -103,7 +103,7 @@
       </div>
     </header>
 
-    <div class="flex flex-1 items-center justify-center content-box h-full">
+    <div class="flex flex-1 {$status === STATUS.INITIAL ? 'items-center' : ''} justify-center content-box h-full">
       {#if $status === STATUS.INITIAL}
         <Confirmation />
       {:else if $status === STATUS.SELECTING}
