fd2e300116032b038ce4914671e600ec939c6860
Шеремет Анна Витальевна <sheremet-av@alamics.local>
2019-09-27 13:34:21 +0500

    SSO-158 fixed cities scroll for mibile

--- keycloak-extension/src/main/resources/themes/domru/login/resources/src/Cities/index.svelte
+++ keycloak-extension/src/main/resources/themes/domru/login/resources/src/Cities/index.svelte
@@ -14,9 +14,6 @@
   import Selection from './Selection.svelte';
   import PhoneButton from './PhoneButton.svelte';
 
-  import './selection';
-  import {selectCity, setAllSelected, setSelectedCity} from "./selection";
-
   let search;
 
   const unsubscribeCity = city.subscribe(value => {
@@ -25,7 +22,9 @@
   });
 
   function handleClose() {
-    setAllSelected();
+    status.set(STATUS.CONFIRMED);
+    showModal.set(false);
+    editingStarted.set(false);
   }
 
   function handleInputChange() {
@@ -33,22 +32,27 @@
   }
 
   function handleSelectCity() {
-    const indexOfChosenCity = $allCities.map(obj => obj.name.toLowerCase()).indexOf(search.toLowerCase());
-    if (indexOfChosenCity === - 1) return;
+    if (!$allCities.map(obj => obj.name).includes(search)) return;
 
-    selectCity($allCities[indexOfChosenCity]);
+    city.set(search);
+    Cookie.set('CITY', search);
+    status.set(STATUS.CONFIRMED);
+    showModal.set(false);
+    editingStarted.set(false);
   }
 </script>
 
 {#if $showModal}
   <div
-    class="flex flex-col fixed w-screen h-screen bg-white inset-0 py-4 md:py-6 xl:py-8 {$status === STATUS.INITIAL && 'opacity-90'} scrollable-container overflow-x-hidden overflow-y-auto custom-scroll"
+    class="flex flex-col fixed w-screen h-screen bg-white inset-0 p-4 sm:px-6
+    md:py-6 lg:px-8 xl:py-8 xl:px-6 {$status === STATUS.INITIAL && 'opacity-90'}"
     id="location-selection-window">
-    <header id="cities-header" class="flex items-center pb-4 px-4 sm:px-6 lg:px-8 xl:px-6">
+    <header id="cities-header" class="flex items-center pb-4">
       <div class="w-full flex justify-between items-center">
-        <a href="https://b2b.domru.ru/" class={$status === STATUS.SELECTING && 'hidden sm:block'}>
+        <a href="/auth" class={$status === STATUS.SELECTING && 'hidden sm:block'}>
           <div
-            class="h-30px w-60px md:h-10 md:w-20 xl:h-16 xl:w-32 bg-contain bg-no-repeat logo logo--domru" />
+            class="h-30px w-60px md:h-10 md:w-20 xl:h-16 xl:w-32 bg-contain
+            bg-no-repeat logo logo--domru" />
         </a>
 
         {#if $status === STATUS.SELECTING}
@@ -99,7 +103,7 @@
       </div>
     </header>
 
-    <div class="flex flex-1 {$status === STATUS.INITIAL ? 'items-center' : ''} justify-center content-box h-full scrollable-container overflow-x-hidden overflow-y-auto mt-4 xl:mt-17">
+    <div class="flex flex-1 {$status === STATUS.INITIAL ? 'items-center' : ''} justify-center content-box h-full scrollable-container overflow-x-hidden overflow-y-auto">
       {#if $status === STATUS.INITIAL}
         <Confirmation />
       {:else if $status === STATUS.SELECTING}
