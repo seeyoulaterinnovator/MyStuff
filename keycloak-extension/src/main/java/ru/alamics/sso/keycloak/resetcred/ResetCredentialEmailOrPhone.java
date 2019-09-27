82986df9853598543fe5c0ed6ab2d4af6d21b8cb
strukov-av <strukov-av@alamics.ru>
2019-09-27 16:10:03 +0500

    SSO-155

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/resetcred/ResetCredentialEmailOrPhone.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/resetcred/ResetCredentialEmailOrPhone.java
@@ -4,16 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.authenticators.browser.AbstractUsernameFormAuthenticator;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.UserModel;
 import org.keycloak.models.utils.FormMessage;
 import org.keycloak.services.messages.Messages;
+import org.keycloak.sessions.AuthenticationSessionModel;
 import ru.alamics.sso.keycloak.auth.AuthBaseClass;
 import ru.alamics.sso.keycloak.auth.UserFind;
 import ru.alamics.sso.keycloak.resetcred.factory.ResetFactory;
 import ru.alamics.sso.keycloak.resetcred.factory.ResetFactoryImpl;
 import ru.alamics.sso.keycloak.resetcred.type.ResetType;
-import ru.alamics.sso.registration.model.User;
-import ru.alamics.sso.registration.rias.RiasService;
+import ru.alamics.sso.registration.rias.exception.RiasCheckException;
+import ru.alamics.sso.registration.rias.port.RiasApiService;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -22,20 +22,18 @@ import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
 import java.util.Collections;
 
-import static ru.alamics.sso.registration.model.UserConstants.ATTR_PHONE_NAME;
-
 
 @Slf4j
 public class ResetCredentialEmailOrPhone extends AuthBaseClass {
 
     private KeycloakSession session;
-    private final RiasService riasService;
+    private RiasApiService riasApiService;
 
-    ResetCredentialEmailOrPhone (KeycloakSession session) {
+    ResetCredentialEmailOrPhone(KeycloakSession session) {
         this.session = session;
         try {
             InitialContext context = new InitialContext();
-            riasService = (RiasService) context.lookup("java:global/domru-sso/" + RiasService.class.getSimpleName());
+            riasApiService = (RiasApiService) context.lookup("java:global/domru-sso/" + RiasApiService.class.getSimpleName());
             log.info("Got riasService from context");
         } catch (NamingException e) {
             log.error(e.getMessage(), e);
@@ -44,19 +42,21 @@ public class ResetCredentialEmailOrPhone extends AuthBaseClass {
     }
 
     @Override
-    public void authenticate (AuthenticationFlowContext context) {
+    public void authenticate(AuthenticationFlowContext context) {
         var user = context.getUser();
         var resetType = ResetType.EMAIL;
         var authenticationSession = context.getAuthenticationSession();
         var username = authenticationSession.getAuthNote(AbstractUsernameFormAuthenticator.ATTEMPTED_USERNAME);
 
-        if(user == null) {
-            username = username.replaceAll("\\D",  "");
+        if (user == null && username.startsWith("+7")) {
+            username = username.replaceAll("\\D", "");
             var userFind = new UserFind(this.session);
             user = userFind.getUserByPhone(username);
-            username = user.getUsername();
-            authenticationSession.setAuthNote(AbstractUsernameFormAuthenticator.ATTEMPTED_USERNAME, user.getEmail());
-            context.getHttpRequest().getDecodedFormParameters().replace("username", Collections.singletonList(user.getEmail()));
+            if (user != null) {
+                username = user.getUsername();
+                authenticationSession.setAuthNote(AbstractUsernameFormAuthenticator.ATTEMPTED_USERNAME, user.getEmail());
+                context.getHttpRequest().getDecodedFormParameters().replace("username", Collections.singletonList(user.getEmail()));
+            }
         }
 
         if (checkRias(context)) {
@@ -73,22 +73,24 @@ public class ResetCredentialEmailOrPhone extends AuthBaseClass {
 
 
     @Override
-    public void action (AuthenticationFlowContext context) {
+    public void action(AuthenticationFlowContext context) {
         context.getUser().setEmailVerified(true);
         context.success();
     }
 
     private boolean checkRias(AuthenticationFlowContext context) {
-        UserModel userModel = context.getUser();
-        if (userModel == null) {
-            return false;
-        }
-        User.UserBuilder userBuilder = User.builder().email(userModel.getEmail());
-        if (!userModel.getAttribute(ATTR_PHONE_NAME).isEmpty()){
-            userBuilder.phone(userModel.getAttribute(ATTR_PHONE_NAME).get(0));
-        }
-        User user = userBuilder.build();
-        if (!riasService.checkEmail(user) && !riasService.checkPhone(user)) {
+        AuthenticationSessionModel authenticationSession = context.getAuthenticationSession();
+        String username = authenticationSession.getAuthNote(AbstractUsernameFormAuthenticator.ATTEMPTED_USERNAME);
+
+        try {
+            if (username.startsWith("+7")) {
+                username = username.replaceAll("\\D", "");
+            }
+            if (username == null || !riasApiService.checkParam(username)) {
+                return false;
+            }
+        } catch (RiasCheckException rce) {
+            log.error("RIAS check service", rce);
             return false;
         }
 
