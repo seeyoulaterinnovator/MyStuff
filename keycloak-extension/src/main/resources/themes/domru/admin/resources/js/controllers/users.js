2129800bd46c4bd7011acef16f58aa95aee148c1
strukov-av <strukov-av@alamics.ru>
2019-10-08 17:19:24 +0500

    SSO-211/добавлены кнопки

--- keycloak-extension/src/main/resources/themes/domru/admin/resources/js/controllers/users.js
+++ keycloak-extension/src/main/resources/themes/domru/admin/resources/js/controllers/users.js
@@ -405,6 +405,22 @@ module.controller('UserListCtrl', function ($scope, realm, User, UserSearchState
         })
     };
 
+    $scope.downloadTemplateXlsx = function () {
+        let payload = {
+            type: 'xlsx',
+            userParameters: [
+                "FIRST_NAME",
+                "EMAIL",
+                "PHONE",
+                "ORGANIZATION",
+                "ROLE",
+                "SYSTEM"
+            ],
+            userIds: ["XXXX-XXXX-XXXX-XXXX-XXXX-XXXX-XXXX"]
+    };
+        $scope.exportTemplateXlsx(payload)
+    };
+
     $scope.exportXlsx = function () {
         let payload = {
             type: 'xlsx',
@@ -421,29 +437,49 @@ module.controller('UserListCtrl', function ($scope, realm, User, UserSearchState
             ],
             userIds: $scope.users.filter(user => user.active).map(user => user.id)
         };
+        $scope.exportTemplateXlsx(payload)
+    };
+
+    $scope.exportTemplateXlsx = function (payload) {
         var linkElement = document.createElement('a');
         $http.post(`${authUrl}/realms/master/users-toms/downloadUsers`, payload, {
             headers: {'Accept': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8', 'Content-Type': 'application/json'},
             responseType: 'arraybuffer'
         }).then((response) => {
             var headers = response.headers();
-            var filename = 'users_info.xlsx';
-            var contentType = headers['content-type'];
-            var blob = new Blob([response.data], {type: contentType});
-            var url = window.URL.createObjectURL(blob, {
-                type: 'data:attachment/xlsx'
-            });
-            window.open(url);
-            linkElement.setAttribute('href', url);
-            linkElement.setAttribute("download", filename);
+        var filename = 'users_info.xlsx';
+        var contentType = headers['content-type'];
+        var blob = new Blob([response.data], {type: contentType});
+        var url = window.URL.createObjectURL(blob, {
+            type: 'data:attachment/xlsx'
+        });
 
-            var clickEvent = new MouseEvent("click", {
-                "view": window,
-                "bubbles": true,
-                "cancelable": false
-            });
-            linkElement.dispatchEvent(clickEvent);
-        })
+        linkElement.setAttribute('href', url);
+        linkElement.setAttribute("download", filename);
+
+        var clickEvent = new MouseEvent("click", {
+            "view": window,
+            "bubbles": true,
+            "cancelable": false
+        });
+        linkElement.dispatchEvent(clickEvent);
+    })
+    };
+
+    $scope.downloadTemplateCSV = function () {
+        let payload = {
+            type: 'csv',
+            userParameters: [
+                "FIRST_NAME",
+                "EMAIL",
+                "PHONE",
+                "ORGANIZATION",
+                "ROLE",
+                "SYSTEM"
+            ],
+            userIds: ["XXXX-XXXX-XXXX-XXXX-XXXX-XXXX-XXXX"]
+        };
+        $scope.exportTemplateCSV(payload)
     };
 
     $scope.exportCSV = function () {
@@ -462,26 +498,30 @@ module.controller('UserListCtrl', function ($scope, realm, User, UserSearchState
             ],
             userIds: $scope.users.filter(user => user.active).map(user => user.id)
         };
+        $scope.exportTemplateCSV(payload)
+    };
+
+    $scope.exportTemplateCSV = function (payload) {
         var linkElement = document.createElement('a');
         $http.post(`${authUrl}/realms/master/users-toms/downloadUsers`, payload, {
             headers: {'Accept': 'application/octet-stream;charset=UTF-8', 'Content-Type': 'application/json'}
         }).then((response) => {
             var headers = response.headers();
-            var filename = 'users_info.csv';
-            var contentType = headers['content-type'];
-            var blob = new Blob(["\ufeff", response.data], {type: contentType});
-            var url = window.URL.createObjectURL(blob);
+        var filename = 'users_info.csv';
+        var contentType = headers['content-type'];
+        var blob = new Blob(["\ufeff", response.data], {type: contentType});
+        var url = window.URL.createObjectURL(blob);
 
-            linkElement.setAttribute('href', url);
-            linkElement.setAttribute("download", filename);
+        linkElement.setAttribute('href', url);
+        linkElement.setAttribute("download", filename);
 
-            var clickEvent = new MouseEvent("click", {
-                "view": window,
-                "bubbles": true,
-                "cancelable": false
-            });
-            linkElement.dispatchEvent(clickEvent);
-        })
+        var clickEvent = new MouseEvent("click", {
+            "view": window,
+            "bubbles": true,
+            "cancelable": false
+        });
+        linkElement.dispatchEvent(clickEvent);
+    })
     };
 
     $scope.nextPage = function () {
