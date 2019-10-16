0923a9bc341d197576a924f27b812ac4812b96f4
Nicky <sheremet-av@itrev.ru>
2019-10-16 13:00:20 +0500

    SSO-229 fixed cities scroll ff

--- keycloak-extension/src/main/resources/themes/domru/login/resources/src/Cities/index.svelte
+++ keycloak-extension/src/main/resources/themes/domru/login/resources/src/Cities/index.svelte
@@ -42,7 +42,7 @@
 
 {#if $showModal}
   <div
-    class="flex flex-col fixed w-screen h-screen bg-white inset-0 py-4 md:py-6 xl:py-8 {$status === STATUS.INITIAL && 'opacity-90'} scrollable-container overflow-x-hidden overflow-y-auto"
+    class="flex flex-col fixed w-screen h-screen bg-white inset-0 py-4 md:py-6 xl:py-8 {$status === STATUS.INITIAL && 'opacity-90'} scrollable-container overflow-x-hidden overflow-y-auto custom-scroll"
     id="location-selection-window">
     <header id="cities-header" class="flex items-center pb-4 px-4 sm:px-6 lg:px-8 xl:px-6">
       <div class="w-full flex justify-between items-center">
