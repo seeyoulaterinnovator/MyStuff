3fafd8d2ee8f14aab9cb3732668371e167024e59
Плюснин Дмитрий Анатольевич <plusnin-da@alamics.ru>
2019-11-29 18:40:45 +0500

    tbapi SIT

--- sso-business-logic/src/main/java/ru/alamics/sso/registration/tbapi/model/TbapiConnectConfig.java
+++ sso-business-logic/src/main/java/ru/alamics/sso/registration/tbapi/model/TbapiConnectConfig.java
@@ -16,8 +16,8 @@ public class TbapiConnectConfig {
     public static TbapiConnectConfig getStaticConfig() {
         if (instance == null) {
             instance = new TbapiConnectConfig();
-            instance.setHost("tb-app01.int.bss.loc");
-            instance.setPort(26300);
+            instance.setHost("10.121.10.41");
+            instance.setPort(26800);
             instance.setAppname("SSP");
             instance.setUsername("anonymous");
             instance.setPath("/api/v1/customerManagement/customerAccount");
