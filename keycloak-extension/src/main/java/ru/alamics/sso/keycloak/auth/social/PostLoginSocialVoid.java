c2ae62b5ff0111a7d87999ae728bf504f6ec7d64
Плюснин Дмитрий Анатольевич <plusnin-da@alamics.ru>
2020-01-29 15:00:26 +0500

    social post login customer window

---
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/social/PostLoginSocialVoid.java
@@ -0,0 +1,109 @@
+package ru.alamics.sso.keycloak.auth.social;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.*;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.List;
+
+import static ru.alamics.sso.registration.model.UserConstants.AUTH_FORM_SUCCESS;
+
+public class PostLoginSocialVoid implements Authenticator, AuthenticatorFactory {
+
+    public static final String PROVIDER_ID = "post-login-social-void";
+
+    private static final AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
+            AuthenticationExecutionModel.Requirement.REQUIRED,
+            AuthenticationExecutionModel.Requirement.DISABLED
+    };
+    @Override
+    public String getDisplayType () {
+        return "Post Login Social Void";
+    }
+
+    @Override
+    public String getReferenceCategory () {
+        return null;
+    }
+
+    @Override
+    public boolean isConfigurable () {
+        return false;
+    }
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices () {
+        return REQUIREMENT_CHOICES;
+    }
+
+    @Override
+    public boolean isUserSetupAllowed () {
+        return false;
+    }
+
+    @Override
+    public String getHelpText () {
+        return "";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties () {
+        return null;
+    }
+
+    @Override
+    public Authenticator create (KeycloakSession session) {
+
+        return this;
+    }
+
+    @Override
+    public void init (Config.Scope config) {
+
+    }
+
+    @Override
+    public void postInit (KeycloakSessionFactory factory) {
+
+    }
+
+    @Override
+    public void close () {
+
+    }
+
+    @Override
+    public String getId () {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    public void authenticate(AuthenticationFlowContext context) {
+
+        context.getAuthenticationSession().setAuthNote(AUTH_FORM_SUCCESS, "1");
+        context.success();
+    }
+
+    @Override
+    public void action(AuthenticationFlowContext context) {
+
+    }
+
+    @Override
+    public boolean requiresUser() {
+        return false;
+    }
+
+    @Override
+    public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
+        return false;
+    }
+
+    @Override
+    public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
+
+    }
+}
