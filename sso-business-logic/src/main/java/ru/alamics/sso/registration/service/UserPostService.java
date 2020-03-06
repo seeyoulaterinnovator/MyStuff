97da9ece878fff62ce134b0aa6a1838fe8db64c0
strukov-av <strukov-av@alamics.ru>
2020-03-06 11:48:41 +0500

    SSO-362/исправлено назначение сис ролей при саморегистрации

--- sso-business-logic/src/main/java/ru/alamics/sso/registration/service/UserPostService.java
+++ sso-business-logic/src/main/java/ru/alamics/sso/registration/service/UserPostService.java
@@ -141,8 +141,9 @@ public class UserPostService {
                 ExternalSystemRoleRequest systemRole = new ExternalSystemRoleRequest();
                 systemRole.setUserPostId(userPost.getId());
                 systemRole.setSystemRoleId(systemRoleDto.getId());
-                return addSystemRole(systemRole);
+                userPost = addSystemRole(systemRole);
             }
+            return userPost;
         } catch (NotFoundException e) {
             log.error(e.getMessage(), e);
         }
