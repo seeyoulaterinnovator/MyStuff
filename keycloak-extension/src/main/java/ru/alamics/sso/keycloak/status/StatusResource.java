b0dd08bbe4a24e0ba08e460bad4138be0ddff1d1
Плюснин Дмитрий Анатольевич <plusnin-da@alamics.ru>
2020-03-11 18:54:48 +0500

    health status

---
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/status/StatusResource.java
@@ -0,0 +1,33 @@
+package ru.alamics.sso.keycloak.status;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jboss.resteasy.annotations.cache.NoCache;
+import org.keycloak.models.KeycloakSession;
+import ru.alamics.sso.keycloak.response.JsonResponse;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Slf4j
+public class StatusResource {
+
+    protected KeycloakSession session;
+
+    public StatusResource(KeycloakSession session) {
+        this.session = session;
+    }
+
+    @GET
+    @Path("health")
+    @NoCache
+    @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+    public Response getStatus() {
+
+        return JsonResponse.success()
+                .addResult("status", 1)
+                .build();
+    }
+}
