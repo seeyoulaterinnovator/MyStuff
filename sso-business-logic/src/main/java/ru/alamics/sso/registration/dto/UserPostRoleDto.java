a5d9f4dd5941c321c6fe7948d0133f97d1481186
Плюснин Дмитрий Анатольевич <plusnin-da@alamics.ru>
2020-04-10 23:35:27 +0500

    проблема с сериализацией UserPostResponse

--- sso-business-logic/src/main/java/ru/alamics/sso/registration/dto/UserPostRoleDto.java
+++ sso-business-logic/src/main/java/ru/alamics/sso/registration/dto/UserPostRoleDto.java
@@ -3,17 +3,11 @@ package ru.alamics.sso.registration.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
-public class UserPostRoleDto {
+public class UserPostRoleDto implements Serializable {
     private Long id;
     private String name;
-
-    @Override
-    public String toString() {
-        return "UserPostRoleDto{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
