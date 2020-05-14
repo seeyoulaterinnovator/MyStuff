5dd5fd4fdcd2803d215a39c2141ce60f2babbcc8
hebudumka <hebudumka@hotmail.com>
2020-05-14 16:29:21 +0500

    SSO-441 п20

--- sso-business-logic/src/main/java/ru/alamics/sso/util/validator/EmailValidator.java
+++ sso-business-logic/src/main/java/ru/alamics/sso/util/validator/EmailValidator.java
@@ -1,14 +1,23 @@
 package ru.alamics.sso.util.validator;
 
 import javax.validation.ValidationException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public abstract class EmailValidator {
 
-    public static void validate(String email) {
-        if (email == null || !email.contains("@") || !email.substring(0, 1).matches("([\\w[\\s]])+")
-                || email.substring(0, 1).matches("[\\d]+") || email.contains(" ") ||
-                !email.substring(email.indexOf("@") + 1, email.indexOf("@") + 2).matches("([\\w[\\s]])+")) {
-            throw new ValidationException(String.format("Email is not valid: email=%s", email));
+    private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public static void validate(String emailStr) {
+
+        if (emailStr == null)
+            throw new ValidationException("Email is null");
+
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+
+        if (!matcher.find()) {
+            throw new ValidationException(String.format("Email is not valid: email=%s", emailStr));
         }
     }
 }
