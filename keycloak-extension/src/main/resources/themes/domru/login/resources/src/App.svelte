331c15c3d68f7a28537078502e3d93b114234770
Плюснин Дмитрий Анатольевич <plusnin-da@alamics.ru>
2020-06-18 14:29:06 +0500

    chat

--- keycloak-extension/src/main/resources/themes/domru/login/resources/src/App.svelte
+++ keycloak-extension/src/main/resources/themes/domru/login/resources/src/App.svelte
@@ -1,6 +1,11 @@
 <script>
   import Cities from './Cities/index.svelte';
   import CitiesButton from './Cities/Button.svelte';
+  import Chat from './scripts/chat.svelte';
+
+  const chat = new Chat({
+    target: document.body,
+  });
 
   const cities = new Cities({
     target: document.getElementById('cities-modal'),
