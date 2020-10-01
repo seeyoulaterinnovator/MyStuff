5d2e73c2835a6c92fe28676b4befd8b31de9d2a3
Плюснин Дмитрий Анатольевич <plusnin-da@alamics.ru>
2020-10-01 20:36:42 +0500

    stage chat isProd false

--- keycloak-extension/src/main/resources/themes/domru/login/resources/src/scripts/chat.svelte
+++ keycloak-extension/src/main/resources/themes/domru/login/resources/src/scripts/chat.svelte
@@ -19,7 +19,7 @@
       nickname: 'Пользователь',
       subject: 'Вопросы со страницы авторизации',
       city: Cookie.get('city-domain') || 'yar',
-      isProd: true
+      isProd: false
     });
     chat.attach(chatContent);
   });
