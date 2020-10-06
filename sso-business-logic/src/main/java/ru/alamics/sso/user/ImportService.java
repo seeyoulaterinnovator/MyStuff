b840767f7e71d9cdd72e984d989231395862102d
Плюснин Дмитрий Анатольевич <plusnin-da@alamics.ru>
2020-10-06 19:05:54 +0500

    SSO-513 scheduler

--- sso-business-logic/src/main/java/ru/alamics/sso/user/ImportService.java
+++ sso-business-logic/src/main/java/ru/alamics/sso/user/ImportService.java
@@ -87,7 +87,7 @@ public class ImportService {
                     if (o.getTomsId() == null || o.getTomsId().isEmpty()) {
                         throw new NotFoundException("TomsId is not exist");
                     }
-                    addUserPost(user, o);
+                    //addUserPost(user, o);
                 } catch (FoundException e) {
                     List<Object> errors = new LinkedList<>();
                     e.getResult().forEach((k, v) -> {
@@ -95,7 +95,7 @@ public class ImportService {
                     });
                     o.setErrors(errors.toString().substring(1, errors.toString().length() - 1));
                     countClones.getAndIncrement();
-                } catch (NotFoundException | NotValidException | FoundUserPostException e) {
+                } catch (NotFoundException | NotValidException /*| FoundUserPostException*/ e) {
                     o.setErrors(e.getMessage());
                     log.error("Importing user data is failed. {}", e.getMessage());
                 } finally {
