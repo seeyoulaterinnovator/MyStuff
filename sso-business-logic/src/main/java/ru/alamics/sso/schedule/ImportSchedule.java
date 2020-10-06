84d2e0cc7b2f11e50603d90feff3775887c536d8
Плюснин Дмитрий Анатольевич <plusnin-da@alamics.ru>
2020-10-06 15:55:10 +0500

    SSO-513 scheduler

--- sso-business-logic/src/main/java/ru/alamics/sso/schedule/ImportSchedule.java
+++ sso-business-logic/src/main/java/ru/alamics/sso/schedule/ImportSchedule.java
@@ -52,10 +52,11 @@ public class ImportSchedule {
                 .stream()
                 .filter(o -> o.getImportUserData() != null && !o.getImportUserData().isEmpty())
                 .filter(o -> o.getStatus().equals(ImportUsersReportStatus.AWAITING))
-                .peek(o -> {
-                    o.setStatus(ImportUsersReportStatus.IN_PROGRESS);
-                    importUsersReportRepository.updateImportUsersReport(o);
-                })
+                // TODO раскоментить
+                //.peek(o -> {
+                //    o.setStatus(ImportUsersReportStatus.IN_PROGRESS);
+                //    importUsersReportRepository.updateImportUsersReport(o);
+                //})
                 .collect(Collectors.toList());
         for (ImportUsersReportEntity importUsersReportEntity : importUsersReportEntities) {
             importService.createImportUsers(importUsersReportEntity);
