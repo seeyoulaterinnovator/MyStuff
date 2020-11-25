f693147717cd72b7f1f775b9b6b7e1696f14a118
strukov-av <strukov-av@alamics.ru>
2020-11-25 13:36:05 +0500

    SSO-529 Скрытие регистрации. Оставить только в iframe - Правка переходов между вкладками "Вход" и "Регистрация"

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/SsoFreeMarkerLoginForm.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/SsoFreeMarkerLoginForm.java
@@ -7,23 +7,29 @@ import org.keycloak.broker.provider.BrokeredIdentityContext;
 import org.keycloak.common.util.ObjectUtil;
 import org.keycloak.forms.login.LoginFormsPages;
 import org.keycloak.forms.login.LoginFormsProvider;
+import org.keycloak.forms.login.freemarker.AuthenticatorConfiguredMethod;
 import org.keycloak.forms.login.freemarker.FreeMarkerLoginFormsProvider;
+import org.keycloak.forms.login.freemarker.LoginFormsUtil;
 import org.keycloak.forms.login.freemarker.Templates;
-import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
-import org.keycloak.models.RequiredActionProviderModel;
-import org.keycloak.models.UserModel;
+import org.keycloak.forms.login.freemarker.model.ClientBean;
+import org.keycloak.forms.login.freemarker.model.IdentityProviderBean;
+import org.keycloak.forms.login.freemarker.model.RealmBean;
+import org.keycloak.forms.login.freemarker.model.RequiredActionUrlFormatterMethod;
+import org.keycloak.models.*;
 import org.keycloak.services.ErrorPage;
 import org.keycloak.services.Urls;
 import org.keycloak.services.messages.Messages;
+import org.keycloak.services.resources.LoginActionsService;
 import org.keycloak.theme.BrowserSecurityHeaderSetup;
 import org.keycloak.theme.FreeMarkerException;
 import org.keycloak.theme.FreeMarkerUtil;
 import org.keycloak.theme.Theme;
+import org.keycloak.theme.beans.LocaleBean;
 import org.keycloak.theme.beans.MessageType;
 import org.keycloak.utils.MediaType;
 import ru.alamics.sso.client.ClientService;
 import ru.alamics.sso.keycloak.auth.model.AuthType;
+import ru.alamics.sso.keycloak.auth.model.SsoUrlBean;
 import ru.alamics.sso.keycloak.lookup.Lookup;
 import ru.alamics.sso.registration.model.FormConstants;
 import ru.alamics.sso.util.Util;
@@ -59,14 +65,80 @@ public class SsoFreeMarkerLoginForm extends FreeMarkerLoginFormsProvider {
         clientService = (ClientService) Lookup.lookup(ClientService.class);
     }
 
+    @Override
+    protected void createCommonAttributes(Theme theme, Locale locale, Properties messagesBundle, UriBuilder baseUriBuilder, LoginFormsPages page) {
+        URI baseUri = baseUriBuilder.build();
+        if (accessCode != null) {
+            baseUriBuilder.queryParam(LoginActionsService.SESSION_CODE, accessCode);
+        }
+        URI baseUriWithCodeAndClientId = baseUriBuilder.build();
+
+        if (client != null) {
+            attributes.put("client", new ClientBean(client, baseUri));
+        }
+
+        if (realm != null) {
+            attributes.put("realm", new RealmBean(realm));
+
+            List<IdentityProviderModel> identityProviders = realm.getIdentityProviders();
+            identityProviders = LoginFormsUtil.filterIdentityProviders(identityProviders, session, realm, attributes, formData);
+            attributes.put("social", new IdentityProviderBean(realm, session, identityProviders, baseUriWithCodeAndClientId));
+
+            attributes.put("url", new SsoUrlBean(realm, theme, baseUri, this.actionUri, isFrame()));
+            attributes.put("requiredActionUrl", new RequiredActionUrlFormatterMethod(realm, baseUri));
+
+            if (realm.isInternationalizationEnabled()) {
+                UriBuilder b;
+                if (page != null) {
+                    switch (page) {
+                        case LOGIN:
+                            b = UriBuilder.fromUri(Urls.realmLoginPage(baseUri, realm.getName()));
+                            break;
+                        case X509_CONFIRM:
+                            b = UriBuilder.fromUri(Urls.realmLoginPage(baseUri, realm.getName()));
+                            break;
+                        case REGISTER:
+                            b = UriBuilder.fromUri(Urls.realmRegisterPage(baseUri, realm.getName()));
+                            break;
+                        default:
+                            b = UriBuilder.fromUri(baseUri).path(uriInfo.getPath());
+                            break;
+                    }
+                } else {
+                    b = UriBuilder.fromUri(baseUri)
+                            .path(uriInfo.getPath());
+                }
+
+                if (execution != null) {
+                    b.queryParam(Constants.EXECUTION, execution);
+                }
+
+                if (authenticationSession != null && authenticationSession.getAuthNote(Constants.KEY) != null) {
+                    b.queryParam(Constants.KEY, authenticationSession.getAuthNote(Constants.KEY));
+                }
+
+                attributes.put("locale", new LocaleBean(realm, locale, b, messagesBundle));
+            }
+        }
+        if (realm != null && user != null && session != null) {
+            attributes.put("authenticatorConfigured", new AuthenticatorConfiguredMethod(realm, user, session));
+        }
+    }
+
     private boolean isHideRegistration() {
         final boolean registrationOnlyInFrame = realm.getAttribute(REGISTRATION_ONLY_IN_FRAME_ATTRIBUTE, false);
 
+        final boolean isIframe = isFrame();
+
+        return registrationOnlyInFrame && !isIframe;
+    }
+
+    private boolean isFrame() {
         //Признак того, что вызов формы ведется в iframe
         String referer = session.getContext().getRequestHeaders().getHeaderString("referer");
 
         if (referer == null) {
-            return registrationOnlyInFrame;
+            return false;
         }
 
         try {
@@ -75,14 +147,7 @@ public class SsoFreeMarkerLoginForm extends FreeMarkerLoginFormsProvider {
             log.warn("Referer is not decoded={}", referer);
         }
 
-        final boolean isIframe = referer.contains("iframe=1");
-
-        //Установка отметки, что вызов происходит в iframe. Необходимо при переходах между вкладками логина и регистрации внутри iframe
-        if (isIframe) {
-            session.getContext().getUri().getPathParameters(true).add("iframe", "1");
-        }
-
-        return registrationOnlyInFrame && !isIframe;
+        return referer.contains("iframe=1");
     }
 
     private String getRedirectUrl() {
