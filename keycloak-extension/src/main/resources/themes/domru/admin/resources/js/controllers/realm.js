d1264bb22c3a039eadf89457f8dbd5fa731b4229
Chelpanov Andrey <chelpanov-aa@itrev.ru>
2021-05-05 12:42:08 +0500

    SSO-560 Реализовать роль для скрытия кнопок на странице users

--- keycloak-extension/src/main/resources/themes/domru/admin/resources/js/controllers/realm.js
+++ keycloak-extension/src/main/resources/themes/domru/admin/resources/js/controllers/realm.js
@@ -12,6 +12,10 @@ function getAccess(Auth, Current, role) {
 
 function getAccessObject(Auth, Current) {
     return {
+        get viewManageButtons(){
+            return getAccess(Auth, Current, 'view-manage-buttons');
+        },
+
         get editAttributes(){
             return getAccess(Auth, Current, 'edit-attributes');
         },
