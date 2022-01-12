4c90475c5c4dad0d5492c424ad4cf95ece01b376
Петров Николай Юрьевич <petrov-ny@alamics.local>
2022-01-12 17:18:13 +0500

    create-model-window Сделал модальные окна для домру

--- frontend/domru/src/main.js
+++ frontend/domru/src/main.js
@@ -29,6 +29,7 @@ import './css/components/confirmation.css';
 import './css/components/customers.css';
 import './css/components/verification.css';
 import './css/components/chat.css';
+import './css/components/message.css';
 
 const app = new App({
   target: document.body,
