be42ae346f4b87f51931f186f7c781a0d9da3670
Петров Николай Юрьевич <petrov-ny@alamics.local>
2022-01-20 12:45:05 +0500

    SSO-615 Сделал логирование запроса в dadata

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/cities/CitiesResource.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/cities/CitiesResource.java
@@ -135,6 +135,8 @@ public class CitiesResource {
         String url = settingsService.getSettingsStringValue(SettingConstants.URL_DADATA_REQUEST_LOCATION_IP, "master");
         String token = settingsService.getSettingsStringValue(SettingConstants.TOKEN_DADATA, "master");
 
+        log.debug(String.format("Sending request with address %s to dadata", ipAddress));
+
         CityDadataModel cityDadataModel = client.target(url)
                 .queryParam("ip", ipAddress)
                 .queryParam("language", LANGUAGE_RU)
