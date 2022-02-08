49d1e699a08740476522a3fd4d5bf8f03027651c
chelpanov.aa2 <Kintep84@mail.ru>
2022-02-08 12:43:46 +0500

    SSOCTLCM-136 Поравил проливку Убрал с кнопки лишнюю логику

--- keycloak-extension/src/main/resources/themes/domru/admin/resources/js/controllers/realm.js
+++ keycloak-extension/src/main/resources/themes/domru/admin/resources/js/controllers/realm.js
@@ -1724,10 +1724,7 @@ module.controller('RealmSMTPSettingsCtrl', function ($scope, Current, Realm, rea
     };
 
     $scope.reset = function () {
-        $scope.realm = angular.copy(oldCopy);
-        $scope.messengerType = angular.copy(oldCopy.smtpServer["messenger"].split(','));
         window.location.reload();
-        $scope.changed = false;
     };
 
     var initSMTPTest = function () {
