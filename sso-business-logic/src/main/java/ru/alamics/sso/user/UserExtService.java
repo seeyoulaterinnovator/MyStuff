75bd18fa5cd2c84b497e839a69ff2d17ad200ad1
Петров Николай Юрьевич <petrov-ny@alamics.local>
2022-03-10 15:34:59 +0500

    SSO-641 Поправил что теперь можно добавлять роли при создании из ЛК

--- sso-business-logic/src/main/java/ru/alamics/sso/user/UserExtService.java
+++ sso-business-logic/src/main/java/ru/alamics/sso/user/UserExtService.java
@@ -11,6 +11,7 @@ import org.keycloak.models.jpa.entities.UserEntity;
 import org.keycloak.provider.ProviderFactory;
 import org.keycloak.services.resources.admin.AdminAuth;
 import org.keycloak.services.resources.admin.AdminEventBuilder;
+import ru.alamics.sso.jpa.entity.UserPostRoleEntity;
 import ru.alamics.sso.keycloak.facade.UserPostFacade;
 import ru.alamics.sso.keycloak.lookup.Lookup;
 import ru.alamics.sso.registration.FoundException;
@@ -175,7 +176,7 @@ public class UserExtService {
 
 
         if (!Util.isEmpty(request.getTomsId())) {
-            addUserPostLPR(user, request);
+            addUserPostRole(user, request, bss);
         }
 
         createAdminEvent(OperationType.CREATE, user);
@@ -223,11 +224,20 @@ public class UserExtService {
                 .success();
     }
 
-    private void addUserPostLPR(UserModel userModel, UserRequest request) throws NotFoundException, FoundException, FoundUserPostException, NotValidException {
+    private void addUserPostRole(UserModel userModel, UserRequest request, boolean bss) throws NotFoundException, FoundException, FoundUserPostException, NotValidException {
 
         UserPostRequest userPostRequest = UserMapper.toUserPostRequest(userModel, request);
 
-        userPostRequest.setRoleId(DEFAULT_ROLE_ID);
+        if (bss || request.getRoleId() == null) {
+            userPostRequest.setRoleId(DEFAULT_ROLE_ID);
+        }
+        if (request.getRoleId() != null && !bss) {
+            UserPostRoleEntity role = userFindService.getRoleEntity(request.getRoleId());
+            if (role == null) {
+                throw new NotFoundException("Роль не найдена.");
+            }
+            userPostRequest.setRoleId(request.getRoleId());
+        }
 
         UserPostResponse userPostResponse = userPostFacade.save(userPostRequest);
 
