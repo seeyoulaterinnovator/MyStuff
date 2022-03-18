30439555aa191b2bd98d970ec457b4a5aa46e9d2
Петров Николай Юрьевич <petrov-ny@alamics.local>
2022-03-18 15:15:39 +0500

    SSO-642 Расширил поиск чтобы можно было передать Email и он находил точь-в-точь

--- custom-jpa-provider/src/main/java/ru/alamics/sso/jpa/util/CollectionUtils.java
+++ custom-jpa-provider/src/main/java/ru/alamics/sso/jpa/util/CollectionUtils.java
@@ -1,7 +1,6 @@
 package ru.alamics.sso.jpa.util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
 
 import java.util.Collection;
 import java.util.List;
@@ -27,6 +26,6 @@ public class CollectionUtils {
     }
 
     public static boolean isNotEmpty(String str) {
-        return !StringUtils.isEmpty(str);
+        return !isEmpty(str);
     }
 }
