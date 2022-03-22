46324caf62d0760e5f11c5fff66140600ce9ff44
Петров Николай Юрьевич <petrov-ny@alamics.local>
2022-03-22 16:21:26 +0500

    test-iframe

--- frontend/domru/src/Cities/stores.js
+++ frontend/domru/src/Cities/stores.js
@@ -26,7 +26,7 @@ isFirstVisit && fetch('/auth/realms/user/cities/current')
 export const status = writable(
   isFirstVisit ? STATUS.INITIAL : STATUS.SELECTING,
 );
-export const showModal = writable(isFirstVisit);
+export const showModal = writable(true);
 export const editingStarted = writable(false);
 export const allCities = writable([]);
 
