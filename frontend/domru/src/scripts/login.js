8654d1b445300c0334d2dd3d016cde64d8e92bb6
Петров Николай Юрьевич <petrov-ny@alamics.local>
2022-03-23 17:52:13 +0500

    test-iframe

--- frontend/domru/src/scripts/login.js
+++ frontend/domru/src/scripts/login.js
@@ -1,5 +1,5 @@
 import IMask from 'imask';
-import { setButtonAvailability } from './helpers.js';
+import {setButtonAvailability} from './helpers.js';
 import Cookie from 'js-cookie';
 import {WRONG_PASS_REG} from "../constants/passwordCharset";
 
@@ -41,6 +41,7 @@ export default (function() {
   formElement.addEventListener('submit', () => {
     usernameElement.value = dynamicMask.unmaskedValue;
     cityElement.value = Cookie.get('city-domain') || 'yar';
+    Cookie.set('VISITED','0', {sameSite: 'None', secure: document.location.protocol === 'https:'});
     return true;
   });
 
