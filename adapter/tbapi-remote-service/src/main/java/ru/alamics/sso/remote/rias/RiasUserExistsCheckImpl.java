5c10b493005149b5120059dc782ed672408852ed
Петров Николай Юрьевич <petrov-ny@alamics.local>
2022-04-04 11:34:32 +0500

    HotFix_2 Логи на проверку в РИАС при смене пароля

--- adapter/tbapi-remote-service/src/main/java/ru/alamics/sso/remote/rias/RiasUserExistsCheckImpl.java
+++ adapter/tbapi-remote-service/src/main/java/ru/alamics/sso/remote/rias/RiasUserExistsCheckImpl.java
@@ -70,6 +70,9 @@ public class RiasUserExistsCheckImpl implements RiasApiService {
         String namesV = Util.encodeUTF8("data_for_check$c,timestamp,client,client_secret");
         String valuesV = Util.encodeUTF8(param + "," + timestamp + "," + properties.getProperty(CLIENT_NAME) + "," + secretHash);
 
+        log.info("paramsV = " + paramsV);
+        log.info("namesV = " + namesV);
+        log.info("valuesV = " + valuesV);
 
         RiasData response;
 
@@ -80,6 +83,8 @@ public class RiasUserExistsCheckImpl implements RiasApiService {
                     .queryParam("param_values_arr$c", valuesV)
                     .request(MediaType.APPLICATION_XML)
                     .get(RiasData.class);
+
+            log.info("response result {}, status {}, message {}", response.getResult(), response.getStatus(), response.getMessages());
 
         } catch (ProcessingException | WebApplicationException wae) {
             throw new RiasCheckException(wae);
