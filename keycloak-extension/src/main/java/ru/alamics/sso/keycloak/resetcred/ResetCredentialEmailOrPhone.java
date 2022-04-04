9f4108b61cf0e007b7743c535277ace53effe92f
Петров Николай Юрьевич <petrov-ny@alamics.local>
2022-04-04 12:14:15 +0500

    HotFix_2 Логи на проверку в РИАС при смене пароля

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/resetcred/ResetCredentialEmailOrPhone.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/resetcred/ResetCredentialEmailOrPhone.java
@@ -74,7 +74,7 @@ public class ResetCredentialEmailOrPhone extends AbstractAuthenticator {
                 context.getHttpRequest().getDecodedFormParameters().replace("username", Collections.singletonList(userFind.getEmail()));
             }
         }
-        if (user == null && userFind == null && !checkRias(context)) {
+        if (user == null && userFind == null && checkRias(context)) {
             return;
         }
 
