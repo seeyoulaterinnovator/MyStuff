0364784e3a93e9f2510891bfd9bb966ed9f47272
Николай <petrov-ny@itrev.ru>
2023-04-24 19:13:51 +0500

    ADD_NEW_REALM_ER fix front js

--- frontend/ertelecom/src/scripts/login.js
+++ frontend/ertelecom/src/scripts/login.js
@@ -89,7 +89,7 @@ export default (function functionName() {
           errorMessageElement.textContent = "";
           usernameElement.classList.remove("field__input--error");
         } else {
-          errorMessageElement.textContent = "����� �������� E-mail";
+          errorMessageElement.textContent = "Введён неверный E-mail";
           usernameElement.classList.add("field__input--error");
         }
       }
