4694cabd54c1b54cc2762992605890f978da65d9
Артём Нестеров <bocopy555@gmail.com>
2023-06-20 19:26:44 +0500

    Add listener to reset forms https://app.clickup.com/t/861myqqw4

--- frontend/ertelecom/src/scripts/login-reset-password.js
+++ frontend/ertelecom/src/scripts/login-reset-password.js
@@ -20,6 +20,45 @@ export default (function() {
 
   const dynamicMask = iMaskInstancePhoneWithoutPrepareAndEmail(usernameElement);
 
+  /**
+   * Phone Listener Start
+   *
+   * Этот Listener и переменные нужны для того, чтобы
+   * при написании 8 появлялось +7, но при нажатии на букву, +7 заменялась на 8-ку.
+   * То есть, когда пишем цифры с "8", например, "8951", то получим в выводе "+7951",
+   * но если мы введём "8951abc", то получим "8951abc".
+   * Не советую разбираться в коде или фиксить код этого Listener'а
+   */
+  let firstCharInUsernameElement = '';
+  let testOnlyDigitsFirstTime = false;
+  let emailStage = false;
+  usernameElement.addEventListener('input', () => {
+    if (dynamicMask.unmaskedValue.length === 0) {
+      testOnlyDigitsFirstTime = false;
+      emailStage = false;
+    }
+
+    const onlyDigits = /^\d+$/.test(dynamicMask.unmaskedValue);
+
+    if (onlyDigits === true && testOnlyDigitsFirstTime === false) {
+      testOnlyDigitsFirstTime = true;
+      if (dynamicMask.unmaskedValue === '78') {
+        firstCharInUsernameElement = '8';
+        dynamicMask.unmaskedValue = '7';
+      } else if (dynamicMask.unmaskedValue.length === 2) {
+        firstCharInUsernameElement = dynamicMask.unmaskedValue.charAt(1);
+      } else {
+        firstCharInUsernameElement = '7';
+      }
+    }
+
+    if (onlyDigits === false && testOnlyDigitsFirstTime === true && emailStage === false) {
+      dynamicMask.unmaskedValue = firstCharInUsernameElement + dynamicMask.unmaskedValue.slice(1);
+      emailStage = true;
+    }
+  });
+  /* Phone Listener End */
+
   usernameElement.addEventListener('input', () => {
     setButtonAvailability(validate, submitElement);
   });
