2dcaac68e72d37f95c9c338c987c675bb333134a
Артём Нестеров <bocopy555@gmail.com>
2023-06-20 17:12:56 +0500

    Fix login mask https://app.clickup.com/t/861myqqw4

--- frontend/stelecom/src/scripts/login.js
+++ frontend/stelecom/src/scripts/login.js
@@ -1,4 +1,7 @@
-import {iMaskInstancePhoneAndEmail, setButtonAvailability} from './helpers.js';
+import {
+  iMaskInstancePhoneWithoutPrepareAndEmail,
+  setButtonAvailability
+} from './helpers.js';
 import Cookie from 'js-cookie';
 import {WRONG_PASS_REG} from "../constants/passwordCharset";
 
@@ -12,10 +15,49 @@ export default (function() {
   const cityElement = document.getElementById('domain-login');
   submitElement.disabled = true;
 
-  const dynamicMask = iMaskInstancePhoneAndEmail(usernameElement);;
+  const dynamicMask = iMaskInstancePhoneWithoutPrepareAndEmail(usernameElement);;
 
   let isUsernameValid = false;
   let isPasswordExists = false;
+
+  /**
+   * Phone Listener Start
+   *
+   * Этот Listener и переменные нужны для того, чтобы
+   * при написании 8 появлялось +7, но при нажатии на букву, +7 заменялась на 8-ку.
+   * То есть, когда пишем цифры с "8", например, "8951", то получим в выводе "+7951",
+   * но если мы введём "8951abc", то получим "8951abc".
+   * Не советую разбираться в коде или фиксить код этого Listener'а
+   */
+  let firstCharInUsernameElement = '';
+  let testOnlyDigitsFirstTime = false;
+  let emailStage = false;
+  usernameElement.addEventListener('input', () => {
+    if (dynamicMask.unmaskedValue.length === 0) {
+      testOnlyDigitsFirstTime = false;
+      emailStage = false;
+    }
+
+    const onlyDigits = /^\d+$/.test(dynamicMask.unmaskedValue);
+
+    if (onlyDigits === true && testOnlyDigitsFirstTime === false) {
+      testOnlyDigitsFirstTime = true;
+      if (dynamicMask.unmaskedValue === '78') {
+        firstCharInUsernameElement = '8';
+        dynamicMask.unmaskedValue = '7';
+      } else if (dynamicMask.unmaskedValue.length === 2) {
+        firstCharInUsernameElement = dynamicMask.unmaskedValue.charAt(1);
+      } else {
+        firstCharInUsernameElement = '7';
+      }
+    }
+
+    if (onlyDigits === false && testOnlyDigitsFirstTime === true && emailStage === false) {
+      dynamicMask.unmaskedValue = firstCharInUsernameElement + dynamicMask.unmaskedValue.slice(1);
+      emailStage = true;
+    }
+  });
+  /* Phone Listener End */
 
   usernameElement.addEventListener('input', () => {
     setButtonAvailability(validate, submitElement);
