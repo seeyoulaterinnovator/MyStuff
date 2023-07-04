ab04682e01ad3aee18fe6a536230df464d67d4df
OutWorldOG <iaroslav.slabunov@ertelecom.ru>
2023-07-04 13:00:04 +0500

    убрал лишнюю строчку

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/form/new_auth/NewAbstractAuthMailPhoneForm.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/form/new_auth/NewAbstractAuthMailPhoneForm.java
@@ -116,7 +116,6 @@ public abstract class NewAbstractAuthMailPhoneForm extends AbstractUsernameFormA
         this.phoneCallerService = Lookup.lookup(PhoneCallerRemoteService.class, "PhoneCallerService");
         this.session = session;
         this.riasService = Lookup.lookup(RiasService.class);
-        this.session = session;
     }
 
     @Override
