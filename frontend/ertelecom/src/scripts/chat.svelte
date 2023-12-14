40ae9b1f9e4b1352bbc5b2e51c427a6e7b2d32d6
Павлов Илья <pavlov-ip@itrev.ru>
2023-12-14 15:57:14 +0300

    Revert "вернул чат эртелеком к исходному состоянию"
    This reverts commit ca23edd0b8e47592296ce13ca14f65e707234ba8.

--- frontend/ertelecom/src/scripts/chat.svelte
+++ frontend/ertelecom/src/scripts/chat.svelte
@@ -1,12 +1,13 @@
 <script>
   import ErChat from 'er-chat/dist/er-chat.esm';
-  import { onMount } from 'svelte';
+  import {onMount} from 'svelte';
   import Cookie from 'js-cookie';
 
   let chatContent;
   let chatWrapper;
   let isOpen = false;
   let showChat = isFramed === undefined || isFramed === false;
+  let test = isChatHidden;
 
   console.log("isFramed s " + isFramed);
 
@@ -25,22 +26,22 @@
   });
 </script>
 
-{#if showChat}
-  <div class="er-chat er-chat-hidden" class:er-chat-hidden={!isOpen}>
-    <div class="er-chat__header">
-      <div class="er-chat__header__close" on:click={toggleChat} />
-    </div>
-    <div class="er-chat__content" bind:this={chatContent} />
+{#if showChat && test!==null}
+<div class="er-chat er-chat-hidden" class:er-chat-hidden={!isOpen}>
+  <div class="er-chat__header">
+    <div class="er-chat__header__close" on:click={toggleChat} />
   </div>
-  {#if !isOpen}
-    <div id="er-chat-label" class="flex flex-col justify-between er-chat-label" >
-      <div class="flex justify-between w-full items-center flex-basis-auto label-header">
-        <div class="flex flex-1 flex-col mr-6 circle">
-          <div class="circle__icon" />
-        </div>
-        <span>Нужна помощь? <br>С удовольствием поможем!</span>
-      </div>
-      <div class="label__text flex-basis-auto w-full" on:click={toggleChat}>Связаться со специалистом &#8250;</div>
+  <div class="er-chat__content" bind:this={chatContent} />
+</div>
+{#if !isOpen}
+<div id="er-chat-label" class="flex flex-col justify-between er-chat-label" >
+  <div class="flex justify-between w-full items-center flex-basis-auto label-header">
+    <div class="flex flex-1 flex-col mr-6 circle">
+      <div class="circle__icon" />
     </div>
-  {/if}
+    <span>Нужна помощь? <br>С удовольствием поможем!</span>
+  </div>
+  <div class="label__text flex-basis-auto w-full" on:click={toggleChat}>Связаться со специалистом &#8250;</div>
+</div>
+{/if}
 {/if}
