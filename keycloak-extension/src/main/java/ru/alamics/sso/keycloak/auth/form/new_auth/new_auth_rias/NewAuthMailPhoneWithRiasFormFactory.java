0e2336f1b9995b074dce61fe2474f3c21796a5b4
Павлов Илья <pavlov-ip@itrev.ru>
2024-01-18 13:11:57 +0300

    убрал лишнюю проверку риас авторизацию + добавил возможность отключать проверку
    https://app.clickup.com/t/8693cuj3r

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/form/new_auth/new_auth_rias/NewAuthMailPhoneWithRiasFormFactory.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/form/new_auth/new_auth_rias/NewAuthMailPhoneWithRiasFormFactory.java
@@ -20,23 +20,25 @@ public class NewAuthMailPhoneWithRiasFormFactory extends AbstractAuthenticatorFa
     private static final String DISPLAY_NAME = "New two step auth with RIAS";
     private static final String HELP_TEXT = "Login/password or phone with sms or phone code with RIAS";
     private static final AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
-            AuthenticationExecutionModel.Requirement.REQUIRED
+            AuthenticationExecutionModel.Requirement.ALTERNATIVE,
+            AuthenticationExecutionModel.Requirement.DISABLED
     };
 
     private static NewAuthMailPhoneWithRiasForm SINGLETON = null;
 
     @Override
     public Authenticator create(KeycloakSession session) {
+        log.info("Call method create Authenticator");
 
         log.info("Got riasService from context");
         UserFindService userFindService = Lookup.lookup(UserFindService.class);
         log.info("Get RiasAuthProvider");
         RiasService riasService = Lookup.lookup(RiasService.class);
 
-        if (userFindService != null) {
-            log.info("NewAuthMailPhoneWithRiasFormFactory_create_userFindService != null");
-            SINGLETON = new NewAuthMailPhoneWithRiasForm(null, userFindService, session);
-        }
+//        if (userFindService != null) {
+//            log.info("NewAuthMailPhoneWithRiasFormFactory_create_userFindService != null");
+//            SINGLETON = new NewAuthMailPhoneWithRiasForm(null, userFindService, session);
+//        }
 
 
         log.info("Creating AuthMailPhoneForm");
