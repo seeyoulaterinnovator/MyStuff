01be53bcdd43743982950f2497d5f0761051238c
Павлов Илья <pavlov-ip@itrev.ru>
2024-03-27 16:28:23 +0300

    изменил заголовок попапа

--- frontend/domru/src/Message/index.svelte
+++ frontend/domru/src/Message/index.svelte
@@ -221,7 +221,7 @@
       <div class="message__title flex flex-row justify-between items-center gap-4">
         <span>
           {#if $isEmailVer}
-            Подтверждение
+            Подтверждение test
           {:else}
             Подтверждение данных
           {/if}
