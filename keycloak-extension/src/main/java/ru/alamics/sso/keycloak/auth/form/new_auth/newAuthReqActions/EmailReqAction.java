015f4c475a2ad46d4fbb621d5c5d6f5c1db8d890
Павлов Илья <pavlov-ip@itrev.ru>
2024-03-28 09:36:19 +0300

    вернул старую логику createForm

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/form/new_auth/newAuthReqActions/EmailReqAction.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/form/new_auth/newAuthReqActions/EmailReqAction.java
@@ -57,10 +57,12 @@ public class EmailReqAction implements RequiredActionProvider {
         boolean isContainsPhone = user.getAttribute("phone").size() == 0;
         log.info("user.getAttribute(\"phone\") is : " + user.getAttribute("phone"));
         LoginFormsProvider form = context.form();
-        if (!isContainsPhone) {
-            log.info("isContainsPhone is " + isContainsPhone);
-            return form.createForm(BLANK_PAGE);
-        }
-        return form.createForm(UPDATE_PASSWORD_FTL);
+//        if (!isContainsPhone) {
+//            log.info("isContainsPhone is " + isContainsPhone);
+//            return form.createForm(BLANK_PAGE);
+//        }
+//        return form.createForm(UPDATE_PASSWORD_FTL);
+        log.info("isContainsPhone is " + isContainsPhone);
+        return form.createForm(BLANK_PAGE);
     }
 }
