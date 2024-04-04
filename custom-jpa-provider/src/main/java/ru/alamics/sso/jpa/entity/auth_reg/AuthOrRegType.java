337e0dbd54cbe74d73c0584e2b886de213d69d1f
Павлов Илья <pavlov-ip@itrev.ru>
2024-04-04 15:06:48 +0300

    добавил forgotPass в enum

--- custom-jpa-provider/src/main/java/ru/alamics/sso/jpa/entity/auth_reg/AuthOrRegType.java
+++ custom-jpa-provider/src/main/java/ru/alamics/sso/jpa/entity/auth_reg/AuthOrRegType.java
@@ -3,7 +3,8 @@ package ru.alamics.sso.jpa.entity.auth_reg;
 public enum AuthOrRegType {
     LOG_PASS(3, "loginPasswordButton", ""),
     PHONE_CALL(2, "phoneCallButton", "incoming_call_phone_verificator"),
-    SMS_CODE(1, "smsButton", "phone_verificator_sms");
+    SMS_CODE(1, "smsButton", "phone_verificator_sms"),
+    FORGOT_PASS(6, "forgotPass", "");
 
     private final int id;
 
