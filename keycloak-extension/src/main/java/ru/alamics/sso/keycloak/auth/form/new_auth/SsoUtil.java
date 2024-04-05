60f6f8ea53f360c8da980bc062fbde18e8e5c1d0
Павлов Илья <pavlov-ip@itrev.ru>
2024-04-05 11:28:01 +0300

    getAuthOrRegType возвращаю 6

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/form/new_auth/SsoUtil.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/form/new_auth/SsoUtil.java
@@ -196,7 +196,7 @@ public interface SsoUtil {
                 return authOrRegType.getId();
             }
         }
-        return 0;
+        return 6;
 //        throw new AuthOrRegTypeNotFoundException("Auth or Reg Type Not Found");
     }
 }
