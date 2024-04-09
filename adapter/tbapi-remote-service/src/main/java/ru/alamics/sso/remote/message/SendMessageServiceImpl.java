17d441515c14a3525e0cdea181873f38b0f15f0b
Павлов Илья <pavlov-ip@itrev.ru>
2024-04-09 14:31:42 +0300

    изменил текст в смс, убрал host и прочее

--- adapter/tbapi-remote-service/src/main/java/ru/alamics/sso/remote/message/SendMessageServiceImpl.java
+++ adapter/tbapi-remote-service/src/main/java/ru/alamics/sso/remote/message/SendMessageServiceImpl.java
@@ -56,9 +56,11 @@ public class SendMessageServiceImpl implements SendMessageService {
 
     @Override
     public void sendMessageToMessengers(String phone, String message, String realmId, String[] messengerList, String host) throws SendMessageException {
-        String pattern = String.format("Your OTP is: %s.\n\n" +
-                "@%s #%s", message, host, message);
-//        String pattern = message;
+//        без дополнительных данных в смс("Your OTP is..." не срабатывает автоподстановка кода из смс
+//                String pattern = String.format("Your OTP is: %s.\n\n" +
+//                "@%s #%s", message, host, message);
+
+        String pattern = message;
 
         MessageRequest messageRequest = MessageRequest.builder()
                 .userPhone(phone)
