290579e1f936b03c99eee1627967ff8cb72ca9aa
Павлов Илья <pavlov-ip@itrev.ru>
2024-04-11 13:27:07 +0300

    убрал лишние логи из п3

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/event/listener/ExtendedEventListenerProvider.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/event/listener/ExtendedEventListenerProvider.java
@@ -49,75 +49,26 @@ public class ExtendedEventListenerProvider implements EventListenerProvider {
         }
 
         long now = System.currentTimeMillis();
-        log.info("now is " + now);
-
-
-        //  typeId - hardcode для авторизации через МП по отпечатку/коду
-
-
         RealmProvider model = session.realms();
         RealmModel realm = model.getRealm(event.getRealmId());
         UserModel userModel = session.users().getUserById(userId, realm);
+        //  typeId - hardcode для авторизации через МП по отпечатку/коду
         if (EventType.REFRESH_TOKEN.equals(event.getType()) && clientId.equals("app_b2b")) {
             if ((now - Long.parseLong(userModel.getAttribute("authorization_time").get(0)) < TimeUnit.DAYS.toMillis(1))) {
                 return;
             }
-//            if ((now - Long.parseLong(userModel.getAttribute("authorization_time").get(0)) > TimeUnit.DAYS.toMillis(1)) &&
-//                    (userModel.getAttribute("number_of_ref_tokens").contains("0"))) {
                 authorisedUsersService.saveSuccessfulAuthFromEventListener(userId, realmId, clientId, 7);
                 userModel.setSingleAttribute("authorization_time", String.valueOf(now));
-//                userModel.setSingleAttribute("number_of_ref_tokens", "1");
-//            }
+
         } else if (EventType.LOGIN.equals(event.getType()) && clientId.equals("app_b2b")) {
             authorisedUsersService.saveSuccessfulAuthFromEventListener(userId, realmId, clientId, 3);
 
             long hoursToSubtract = 25;
             long millisecondsToSubtract = hoursToSubtract * 3600000; // Перевод часов в миллисекунды
-
             long nowMinus25 = now - millisecondsToSubtract;
 
-            log.info("nowMinus25 is " + nowMinus25);
-
             userModel.setSingleAttribute("authorization_time", String.valueOf(nowMinus25));
-//            userModel.setSingleAttribute("number_of_ref_tokens", "0");
         }
-//        else if ((EventType.REFRESH_TOKEN.equals(event.getType()) && clientId.equals("app_b2b")) && (now - Long.parseLong(userModel.getAttribute("authorization_time").get(0)) > TimeUnit.DAYS.toMillis(1))) {
-//            userModel.setSingleAttribute("number_of_ref_tokens", "0");
-//        }
-
-//        try {
-//            long userAtt = Long.parseLong(userModel.getAttribute("authorization_time").get(0));
-//        }
-//        catch (IndexOutOfBoundsException e) {
-//            log.info("userModel.getAttribute(authorization_time).get(0) = " + userModel.getAttribute("authorization_time").get(0));
-//            userModel.setSingleAttribute("authorization_time", String.valueOf(now));
-//
-//        }
-//        if (userModel.getAttribute("authorization_time").get(0) == null) {
-//            log.info("userModel.setSingleAttribute");
-//            userModel.setSingleAttribute("authorization_time", String.valueOf(now));
-//        }
-
-//        long userAtt = Long.parseLong(userModel.getAttribute("authorization_time").get(0));
-//        log.info("userAtt = " + userAtt);
-
-//        log.info(" now - lastRecordTime > TimeUnit.DAYS.toMillis(1) is : " + (now - lastRecordTime > TimeUnit.DAYS.toMillis(1)));
-
-//        if (lastRecordTime == null || !isSameDay(lastRecordTime, now)) {
-//            log.info("onEvent is called, lastRecordTime != null ");
-////        if (lastRecordTime == null || now - lastRecordTime > TimeUnit.DAYS.toMillis(1)) {
-//            if (EventType.REFRESH_TOKEN.equals(event.getType())) {
-//                log.info("loging REFRESH TOKEN");
-//                authorisedUsersService.saveSuccessfulAuthFromEventListener(userId, realmId, clientId, 7);
-//            } else if (EventType.LOGIN.equals(event.getType()) && clientId.equals("app_b2b")) {
-//                log.info("loging LOGIN APP_b2b");
-//                authorisedUsersService.saveSuccessfulAuthFromEventListener(userId, realmId, clientId, 3);
-//            }
-//            log.info("after lastRecordTimestamps.toString() is " + lastRecordTimestamps.toString());
-//            lastRecordTimestamps.put(key, now);
-//            log.info("before lastRecordTimestamps.toString() is " + lastRecordTimestamps.toString());
-//        }
-
     }
 
 
