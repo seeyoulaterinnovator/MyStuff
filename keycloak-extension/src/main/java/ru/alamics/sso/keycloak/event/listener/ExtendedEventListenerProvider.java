9f3ddd5536158c67885f2af77251a71a335664a6
ichetovkina-ey <iej@alamics.ru>
2024-07-03 17:19:36 +0500

    SSOERTH-15

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/event/listener/ExtendedEventListenerProvider.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/event/listener/ExtendedEventListenerProvider.java
@@ -57,7 +57,7 @@ public class ExtendedEventListenerProvider implements EventListenerProvider {
         switch (event.getType()) {
             case LOGIN: {
                 if(clients.contains(clientId)) {
-                    if(userModel.getFirstAttribute("login_first") != null) {
+                    if(userModel.getFirstAttribute("login_first") != null && !userModel.getFirstAttribute("login_first").equals(clientId)) {
                         authorisedUsersService.saveSuccessfulAuthFromEventListener(userId, realmId, clientId, 8);
                     } else {
                         if (clientId.equals("app_b2b")) {
