10d8bb394c087ea83e582ab9d5d593e3c8cc36b2
korotaev-my <korotaev-my@it-resource.team>
2024-07-15 18:31:26 +0500

    SSOERTH-19 | подключение чата только вне iframe

--- frontend/domru/src/scripts/chat.svelte
+++ frontend/domru/src/scripts/chat.svelte
@@ -4,29 +4,31 @@
   import Cookie from "js-cookie";
 
   onMount(() => {
-    const initialize = () => {
-      customConfig.subscribe(config => {
-        if (!window.B2B_CHAT_WIDGET_PARAMS && config.isLoaded) {
-          window.B2B_CHAT_WIDGET_PARAMS = {
-            server: config.b2bChatWidgetServer,
-            userData: {
-              citydomain: Cookie.get('city-domain') || 'interzet'
-            }
-          };
-          const element = document.createElement('script');
-          element.src = config.b2bChatWidgetUrl;
-          document.head.appendChild(element);
-        }
-      });
-    };
-    if(document.readyState === 'complete') {
-      initialize();
-    } else {
-      document.addEventListener('readystatechange', () => {
-        if(document.readyState === 'complete') {
-          initialize();
-        }
-      });
+    if(window.parent === window) {
+      const initialize = () => {
+        customConfig.subscribe(config => {
+          if (!window.B2B_CHAT_WIDGET_PARAMS && config.isLoaded) {
+            window.B2B_CHAT_WIDGET_PARAMS = {
+              server: config.b2bChatWidgetServer,
+              userData: {
+                citydomain: Cookie.get('city-domain') || 'interzet'
+              }
+            };
+            const element = document.createElement('script');
+            element.src = config.b2bChatWidgetUrl;
+            document.head.appendChild(element);
+          }
+        });
+      };
+      if(document.readyState === 'complete') {
+        initialize();
+      } else {
+        document.addEventListener('readystatechange', () => {
+          if(document.readyState === 'complete') {
+            initialize();
+          }
+        });
+      }
     }
   });
 </script>
