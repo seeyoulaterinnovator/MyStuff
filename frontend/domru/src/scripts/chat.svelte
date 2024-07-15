bb43c025e0a6e91e358b12ada37497a94694e181
korotaev-my <korotaev-my@it-resource.team>
2024-07-15 12:03:24 +0500

    SSOERTH-19 | настройки контура для виджета чата

--- frontend/domru/src/scripts/chat.svelte
+++ frontend/domru/src/scripts/chat.svelte
@@ -1,19 +1,20 @@
 <script>
   import {onMount} from 'svelte';
-  import {b2bChatWidgetUrl} from "../Cities/stores";
+  import {customConfig} from "../Config/stores";
   import Cookie from "js-cookie";
 
   onMount(() => {
     const initialize = () => {
-      b2bChatWidgetUrl.subscribe(src => {
-        if (!window.B2B_CHAT_WIDGET_PARAMS && src) {
+      customConfig.subscribe(config => {
+        if (!window.B2B_CHAT_WIDGET_PARAMS && config.isLoaded) {
           window.B2B_CHAT_WIDGET_PARAMS = {
+            server: config.b2bChatWidgetServer,
             userData: {
               citydomain: Cookie.get('city-domain') || 'interzet'
             }
           };
           const element = document.createElement('script');
-          element.src = src;
+          element.src = config.b2bChatWidgetUrl;
           document.head.appendChild(element);
         }
       });
