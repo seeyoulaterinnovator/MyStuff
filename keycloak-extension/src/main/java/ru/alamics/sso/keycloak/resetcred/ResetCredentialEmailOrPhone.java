855af4ebee0a7c14a1ff189963dfd8cdb12dc527
ichetovkina-ey <iej@alamics.ru>
2024-07-23 19:05:55 +0500

    SSOERTH-17 | кейс 3

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/resetcred/ResetCredentialEmailOrPhone.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/resetcred/ResetCredentialEmailOrPhone.java
@@ -135,7 +135,8 @@ public class ResetCredentialEmailOrPhone extends AbstractAuthenticator {
     public void action(AuthenticationFlowContext context) {
         AuthenticationSessionModel authenticationSession = context.getAuthenticationSession();
         log.info("action verify-email-by-reset context.getUser() = {}", context.getUser().getEmail());
-        String username = authenticationSession.getAuthNote("verifyEmail");
+//        String username = authenticationSession.getAuthNote("verifyEmail");
+        String username = context.getHttpRequest().getDecodedFormParameters().getFirst("verifyEmail");
         log.info("action verify-email-by-reset username = {}", username);
         if(username != null && context.getUser().getEmail() != null && username.equals(context.getUser().getEmail())){
             context.getUser().setEmailVerified(true);
