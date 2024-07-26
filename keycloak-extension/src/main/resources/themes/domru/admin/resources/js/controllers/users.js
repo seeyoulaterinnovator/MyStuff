deebfeed81c9f4e906e9fb9653bd0ff9a28f5639
ichetovkina-ey <iej@alamics.ru>
2024-07-26 17:12:26 +0500

    кейс 4

--- keycloak-extension/src/main/resources/themes/domru/admin/resources/js/controllers/users.js
+++ keycloak-extension/src/main/resources/themes/domru/admin/resources/js/controllers/users.js
@@ -1217,7 +1217,7 @@ module.controller('UserCredentialsCtrl', function ($scope, realm, user, $route, 
             Dialog.message("Cannot send email", "You must save your current changes before you can send an email");
             return;
         }
-        if(user.attributes.getNamedItem("phone") == null && user.emailVerified === false){
+        if(user.emailVerified === false){
             Dialog.message("Cannot send email", "В Учётной записи клиента не подтверждена почта и не указан номер телефона, письмо не отправлено");
         } else {
             Dialog.confirm('Send Email', 'Are you sure you want to send email to user?', function () {
