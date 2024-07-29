9ba375cf523310c1a63a6934fd3f88bcf037be6f
ichetovkina-ey <iej@alamics.ru>
2024-07-29 11:29:49 +0500

    SSOERTH-17 | кейс 4

--- keycloak-extension/src/main/resources/themes/domru/admin/resources/js/controllers/users.js
+++ keycloak-extension/src/main/resources/themes/domru/admin/resources/js/controllers/users.js
@@ -1219,9 +1219,9 @@ module.controller('UserCredentialsCtrl', function ($scope, realm, user, $route, 
         }
         if(user.emailVerified === false){
             if(!!user.attributes.phone){
-                Dialog.message("Cannot send email", "В Учётной записи клиента не подтверждена почта и не указан номер телефона, письмо не отправлено");
-            } else {
                 Dialog.message("Cannot send email", "Письмо не може быть отправлено, почта в Учётной записи не подтверждена");
+            } else {
+                Dialog.message("Cannot send email", "В Учётной записи клиента не подтверждена почта и не указан номер телефона, письмо не отправлено");
             }
         } else {
             Dialog.confirm('Send Email', 'Are you sure you want to send email to user?', function () {
