243f107523633b12b4c1aedb04d9bd317ad96490
korotaev-my <korotaev-my@it-resource.team>
2024-08-14 15:47:05 +0500

    SSOERTH-16 | добавил не хватающий лог для статистики

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/rest/RestSmsOrPhoneCallPasswordValidator.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/rest/RestSmsOrPhoneCallPasswordValidator.java
@@ -2,14 +2,19 @@ package ru.alamics.sso.keycloak.auth.rest;
 
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.AuthenticationFlowError;
+import org.keycloak.authentication.Authenticator;
 import org.keycloak.authentication.authenticators.directgrant.ValidatePassword;
 import org.keycloak.events.Errors;
 import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.UserCredentialModel;
 import org.keycloak.models.UserModel;
+import ru.alamics.sso.jpa.entity.auth_reg.AuthOrRegType;
 import ru.alamics.sso.keycloak.facade.CachedUserPostFacade;
 import ru.alamics.sso.keycloak.lookup.Lookup;
+import ru.alamics.sso.keycloak.registration.mapper.UserModelUserMapper;
 import ru.alamics.sso.registration.dto.UserPostResponse;
+import ru.alamics.sso.registration.service.AuthorisedUsersService;
 
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -26,6 +31,14 @@ public class RestSmsOrPhoneCallPasswordValidator extends ValidatePassword {
     private static final AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
             AuthenticationExecutionModel.Requirement.REQUIRED
     };
+
+    private AuthorisedUsersService authorisedUsersService;
+
+    @Override
+    public Authenticator create(KeycloakSession session) {
+        this.authorisedUsersService = Lookup.lookup(AuthorisedUsersService.class);
+        return super.create(session);
+    }
 
     @Override
     public void authenticate(AuthenticationFlowContext context) {
@@ -45,6 +58,14 @@ public class RestSmsOrPhoneCallPasswordValidator extends ValidatePassword {
                 Response challengeResponse = errorResponse(Response.Status.UNAUTHORIZED.getStatusCode(), "invalid_grant", "Invalid user credentials");
                 context.failure(AuthenticationFlowError.INVALID_USER, challengeResponse);
                 return;
+            } else {
+                authorisedUsersService.saveSuccessfulAuth(
+                        user,
+                        UserModelUserMapper.mapToUser(context.getUser()),
+                        context.getRealm().getName(),
+                        context.getAuthenticationSession().getClient().getClientId(),
+                        AuthOrRegType.LOG_PASS.getId()
+                );
             }
         }
 
