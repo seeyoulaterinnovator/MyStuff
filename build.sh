d6c50a47d140ba4d0d9d6b8ab4f4f3c27b75b50d
korotaev-my <korotaev-my@it-resource.team>
2024-09-12 12:19:58 +0500

    SSOERTH-49

--- build.sh
+++ build.sh
@@ -1,31 +1,24 @@
 #!/usr/bin/env bash
 baseDir="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
-
-NPM_REGISTRY=https://registry.npmjs.org
-if [[ -z "${KC_ADMIN_VITE_ENABLED}" ]]
-then
-  export NPM_REGISTRY="https://registry.npmjs.org"
-fi
-
-npm install --registry=$NPM_REGISTRY -g pnpm
-
 set -x
 set -e
 
+npm install -g pnpm
+
 cd $baseDir/frontend/domru
-npm install --registry=$NPM_REGISTRY
+npm install
 npm run build
 
 cd $baseDir/frontend/ertelecom
-npm install --registry=$NPM_REGISTRY
+npm install
 npm run build
 
 cd $baseDir/frontend/stelecom
-npm install --registry=$NPM_REGISTRY
+npm install
 npm run build
 
 cd $baseDir/frontend-admin
-pnpm install --registry=$NPM_REGISTRY
+pnpm install
 cd $baseDir/frontend-admin/js/apps/admin-ui
 pnpm run build
 
