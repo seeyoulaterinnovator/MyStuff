5af9921abc2cc72e44ad0e215eb9ec28cdcfb10c
Andrey Barsukov <barsukov-as@it-resource.team>
2024-10-14 12:09:15 +0500

    SSOERTH-151 | Правка стилей

--- frontend-admin/js/apps/admin-ui/src/components/users/UserDataTableCustomSearchForm.tsx
+++ frontend-admin/js/apps/admin-ui/src/components/users/UserDataTableCustomSearchForm.tsx
@@ -1,14 +1,8 @@
 import type { CustomUserQuery } from "@keycloak/keycloak-admin-client/lib/resources/custom/users";
-import {
-  Button,
-  TextInput,
-  ToolbarGroup,
-  ToolbarItem,
-} from "@patternfly/react-core";
+import { Button, TextInput, ToolbarItem } from "@patternfly/react-core";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { Form } from "react-router-dom";
 import { RealmSimpleSelector } from "../custom/realm-simple-selector/RealmSimpleSelector";
 import { useAccess } from "../../context/access/Access";
 import { useCustomConfig } from "../../customLogic/context/CustomConfigContext";
@@ -48,77 +42,83 @@ export function UserDataTableCustomSearchForm({
     reset(customFilters);
   }, [customFilters]);
 
+  if (!isCustomTheme || withHideUserSearchAccess) {
+    return null;
+  }
+
   return (
-    <Form
-      className="pf-v5-u-flex-grow-1"
-      onSubmit={handleSubmit(searchUserWithCustomFilters)}
-    >
-      {isCustomTheme && !withHideUserSearchAccess && (
-        <ToolbarGroup
-          className="pf-m-wrap pf-m-space-items"
-          variant="filter-group"
-          spaceItems={{ default: "spaceItemsSm" }}
+    <>
+      <ToolbarItem
+        className="pf-v5-u-flex-grow-1"
+        style={{ minWidth: "140px" }}
+      >
+        <TextInput
+          id="value"
+          placeholder={t("search")}
+          {...register("search")}
+        />
+      </ToolbarItem>
+      <ToolbarItem
+        className="pf-v5-u-flex-grow-1"
+        style={{ minWidth: "140px" }}
+      >
+        <TextInput
+          id="userId"
+          placeholder={t("searchByUserId")}
+          {...register("searchUser")}
+        />
+      </ToolbarItem>
+      <ToolbarItem
+        className="pf-v5-u-flex-grow-1"
+        style={{ minWidth: "140px" }}
+      >
+        <TextInput
+          id="tomsId"
+          placeholder={t("searchByTomsId")}
+          {...register("searchToms")}
+        />
+      </ToolbarItem>
+      <ToolbarItem
+        className="pf-v5-u-flex-grow-1"
+        style={{ minWidth: "140px" }}
+      >
+        <TextInput
+          id="phone"
+          placeholder={t("searchByPhone")}
+          {...register("searchPhone")}
+        />
+      </ToolbarItem>
+      <ToolbarItem>
+        <Button
+          data-testid="search-user-attribute-btn"
+          variant="primary"
+          type="submit"
+          onClick={() => handleSubmit(searchUserWithCustomFilters)()}
         >
-          <ToolbarItem className="pf-v5-u-flex-grow-1">
-            <TextInput
-              id="value"
-              placeholder={t("search")}
-              {...register("search")}
-            />
-          </ToolbarItem>
-          <ToolbarItem className="pf-v5-u-flex-grow-1">
-            <TextInput
-              id="userId"
-              placeholder={t("searchByUserId")}
-              {...register("searchUser")}
-            />
-          </ToolbarItem>
-          <ToolbarItem className="pf-v5-u-flex-grow-1">
-            <TextInput
-              id="tomsId"
-              placeholder={t("searchByTomsId")}
-              {...register("searchToms")}
-            />
-          </ToolbarItem>
-          <ToolbarItem className="pf-v5-u-flex-grow-1">
-            <TextInput
-              id="phone"
-              placeholder={t("searchByPhone")}
-              {...register("searchPhone")}
-            />
-          </ToolbarItem>
-          <ToolbarItem>
-            <Button
-              data-testid="search-user-attribute-btn"
-              variant="primary"
-              type="submit"
-            >
-              {t("search")}
-            </Button>
-          </ToolbarItem>
-          <ToolbarItem>
-            <Button variant="primary" onClick={clearCustomFilters}>
-              {t("viewAllUsers")}
-            </Button>
-          </ToolbarItem>
-          <ToolbarItem>
-            <RealmSimpleSelector
-              id="searchRealm"
-              {...register("searchRealm")}
-              value={watch("searchRealm")}
-              realmsSource="accessible"
-              onChange={(newSearchRealm) => {
-                setValue("searchRealm", newSearchRealm);
-                handleSubmit(() =>
-                  searchUserWithCustomFilters({
-                    searchRealm: newSearchRealm,
-                  }),
-                )();
-              }}
-            />
-          </ToolbarItem>
-        </ToolbarGroup>
-      )}
-    </Form>
+          {t("search")}
+        </Button>
+      </ToolbarItem>
+      <ToolbarItem>
+        <Button variant="primary" onClick={clearCustomFilters}>
+          {t("viewAllUsers")}
+        </Button>
+      </ToolbarItem>
+      <ToolbarItem>
+        <RealmSimpleSelector
+          id="searchRealm"
+          {...register("searchRealm")}
+          value={watch("searchRealm")}
+          realmsSource="accessible"
+          onChange={(newSearchRealm) => {
+            setValue("searchRealm", newSearchRealm);
+            handleSubmit(() =>
+              searchUserWithCustomFilters({
+                searchRealm: newSearchRealm,
+              }),
+            )();
+          }}
+        />
+      </ToolbarItem>
+    </>
   );
 }
--- frontend-admin/js/apps/admin-ui/src/components/users/UserDataTableToolbarItems.tsx
+++ frontend-admin/js/apps/admin-ui/src/components/users/UserDataTableToolbarItems.tsx
@@ -134,7 +134,7 @@ export function UserDataTableToolbarItems({
   const searchItem = () => {
     return (
       <ToolbarGroup
-        className={`"pf-m-wrap" ${isCustomTheme && searchType === "custom" ? "pf-v5-u-w-100" : ""}`}
+        className={`"pf-m-wrap" ${isCustomTheme && searchType === "custom" ? "pf-v5-u-w-100 pf-v5-u-flex-wrap" : ""}`}
         variant="filter-group"
         {...(isCustomTheme && searchType === "custom"
           ? {
