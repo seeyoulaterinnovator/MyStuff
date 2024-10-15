4767ebda8c13f08b4309182920cb85285aa1e44f
Коротаев Максим Юрьевич <korotaev-my@it-resource.team>
2024-10-15 20:29:01 +0500

    Update TraceUtil.java

--- sso-business-logic/src/main/java/ru/alamics/sso/util/TraceUtil.java
+++ sso-business-logic/src/main/java/ru/alamics/sso/util/TraceUtil.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TraceUtil {
     public static boolean isTraceEnabled() {
-        return !StandResolver.isBattle() && Boolean.TRUE.toString().equals(System.getenv("ERTH_TRACE_ENABLED"));
+        return Boolean.TRUE.toString().equals(System.getenv("APP_TRACE_ENABLED"));
     }
 }
