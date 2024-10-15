b6151c4e8e7e99ef3f72a38bc30f7fb9b66065b3
korotaev-my <korotaev-my@it-resource.team>
2024-10-15 15:56:42 +0500

    add stage debug markup

--- keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/SsoFreeMarkerLoginForm.java
+++ keycloak-extension/src/main/java/ru/alamics/sso/keycloak/auth/SsoFreeMarkerLoginForm.java
@@ -33,6 +33,7 @@ import ru.alamics.sso.keycloak.util.MiscUtil;
 import ru.alamics.sso.registration.model.FormConstants;
 import ru.alamics.sso.settings.SettingConstants;
 import ru.alamics.sso.settings.SettingsService;
+import ru.alamics.sso.util.StandResolver;
 import ru.alamics.sso.util.Util;
 
 import java.net.URI;
@@ -209,6 +210,7 @@ public class SsoFreeMarkerLoginForm extends FreeMarkerLoginFormsProvider {
                 putAttribute("lastLoginPhone", getAndRemoveLastLoginPhone());
             }
             putAttribute("restoreButtonLabel", settingsService.getSettingsStringValue(RESTORE_BUTTON_LABEL, realm.getName()));
+            putAttribute("isBattle", StandResolver.isBattle());
 
             if (realm.isInternationalizationEnabled()) {
                 UriBuilder b;
