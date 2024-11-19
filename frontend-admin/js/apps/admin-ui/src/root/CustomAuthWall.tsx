37fab1951a890554fb3e1110a7ce037eecc25d18
korotaev-my <korotaev-my@it-resource.team>
2024-11-19 13:09:13 +0500

    SSOERTH-243

--- frontend-admin/js/apps/admin-ui/src/root/CustomAuthWall.tsx
+++ frontend-admin/js/apps/admin-ui/src/root/CustomAuthWall.tsx
@@ -4,15 +4,10 @@ import { useMatch, useNavigate } from "react-router-dom";
 import { UserRoute } from "../user/routes/User";
 import { toUsers, UsersRoute } from "../user/routes/Users";
 import { RootRoute } from "../routes";
-import { useEffect } from "react";
 import { useRealm } from "../context/realm-context/RealmContext";
-import { useErrorBoundary } from "../context/ErrorBoundary";
-import { useTranslation } from "react-i18next";
 import { AddUserRoute } from "../user/routes/AddUser";
 
 export const CustomAuthWall = ({ children }: any) => {
-  const { t } = useTranslation();
-  const { showBoundary } = useErrorBoundary();
   const navigate = useNavigate();
   const { isCustomTheme } = useCustomConfig();
   const { realm } = useRealm();
@@ -24,16 +19,8 @@ export const CustomAuthWall = ({ children }: any) => {
   const isManagerPages =
     isRootPage || isUsersPage || isUserPage || isAddUserPage;
   const isForbidden = isCustomTheme && isMeInManager && !isManagerPages;
-
-  useEffect(() => {
-    if (isForbidden) {
-      navigate(toUsers({ realm }));
-    }
-  }, []);
-
-  if (isForbidden) {
-    showBoundary(new Error(`${t("forbidden")}. ${t("noAccessResource")}.`));
+  if (isForbidden && !isUsersPage) {
+    navigate(toUsers({ realm }));
   }
-
   return children;
 };
